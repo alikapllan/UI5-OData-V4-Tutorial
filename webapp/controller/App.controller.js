c67072a276efcb4de12e46bb312a1e5ca8b73571
alikapllan <alihkaplan44@gmail.com>
2025-05-07 16:33:05 +0200

    Step 2: Data Access and Client-Server Communication
    https://sapui5.hana.ondemand.com/#/topic/e9de002ddf1e478fbb9232bfba83dcf0

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,6 +1,11 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
-  function (Controller, JSONModel) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
+  ],
+  function (Controller, JSONModel, MessageToast, MessageBox) {
     "use strict";
 
     return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -14,6 +19,24 @@ sap.ui.define(
           oModel = new JSONModel(oJSONData);
 
         this.getView().setModel(oModel, "appView");
+      },
+
+      onRefresh: function () {
+        var oBinding = this.byId("peopleList").getBinding("items");
+
+        if (oBinding.hasPendingChanges()) {
+          MessageBox.error(this._getText("refreshNotPossibleMessage"));
+          return;
+        }
+        oBinding.refresh();
+        MessageToast.show(this._getText("refreshSuccessMessage"));
+      },
+
+      _getText: function (sTextId, aArgs) {
+        return this.getOwnerComponent()
+          .getModel("i18n")
+          .getResourceBundle()
+          .getText(sTextId, aArgs);
       },
     });
   }
