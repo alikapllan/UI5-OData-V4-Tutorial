77f15db75f2697ba5d3ee919d244eacf6342abcb
alikapllan <alihkaplan44@gmail.com>
2025-05-08 22:15:22 +0200

    Step 6: Create and Edit
    new learnt :
    
    -> reported error via OData adds them to MessageModel as technical message
    -> ODataModel.hasPendingChanges() : returns true if there are any changes having not yet been written to the service.
    -> sap.ui.getCore().getMessageManager().removeMessages(aMessages) : remove messages from global message manager so that they dont stack out
    -> oEvt.getParameter("escPressed")
    -> add the $$updateGroupId: 'peopleGroup' parameter to the table. This means that changes in the table are not sent to the service immediately but instead are collected until we explicitly send them
    -> valueLiveUpdate property in Input controls :  That makes sure that the SAPUI5 types validate the field content with each keystroke. (In OV4 based on types in metadata.xml SAPUI5 automatic validates types of INput controls)
    
    https://sapui5.hana.ondemand.com/#/topic/b4f12660538147f8839b05cb03f1d478

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define(
   [
+    "sap/ui/core/Messaging",
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast",
@@ -10,6 +11,7 @@ sap.ui.define(
     "sap/ui/model/FilterType",
   ],
   function (
+    Messaging,
     Controller,
     JSONModel,
     MessageToast,
@@ -25,17 +27,71 @@ sap.ui.define(
       /**
        *  Hook for initializing the controller
        */
-      onInit: function () {
-        let oJSONData = {
+      onInit() {
+        let oMessageModel = Messaging.getMessageModel(),
+          oMessageModelBinding = oMessageModel.bindList(
+            "/",
+            undefined,
+            [],
+            new Filter("technical", FilterOperator.EQ, true)
+          ),
+          oViewModel = new JSONModel({
             busy: false,
+            hasUIChanges: false,
+            usernameEmpty: true,
             order: 0,
-          },
-          oModel = new JSONModel(oJSONData);
+          });
 
-        this.getView().setModel(oModel, "appView");
+        this.getView().setModel(oViewModel, "appView");
+        this.getView().setModel(oMessageModel, "message");
+
+        oMessageModelBinding.attachChange(this.onMessageBindingChange, this);
+        this._bTechnicalErrors = false;
       },
 
-      onRefresh: function () {
+      onCreate() {
+        let oList = this.byId("peopleList"),
+          oBinding = oList.getBinding("items");
+
+        const oContext = oBinding.create({
+          UserName: "",
+          FirstName: "",
+          LastName: "",
+          Age: "18",
+        });
+
+        this._setUIChanges();
+        this.getView().getModel("appView").setProperty("/usernameEmpty", true);
+
+        oList.getItems().some(function (oItem) {
+          if (oItem.getBindingContext() === oContext) {
+            oItem.focus();
+            oItem.setSelected(true);
+            return true;
+          }
+        });
+      },
+
+      onInputChange(oEvt) {
+        if (oEvt.getParameter("escPressed")) {
+          this._setUIChanges();
+        } else {
+          this._setUIChanges(true);
+          if (
+            oEvt
+              .getSource()
+              .getParent()
+              .getBindingContext()
+              .getProperty("UserName")
+          ) {
+            this.getView()
+              .getModel("appView")
+              .setProperty("/usernameEmpty", false);
+          }
+        }
+      },
+
+      onRefresh() {
         let oBinding = this.byId("peopleList").getBinding("items");
 
         if (oBinding.hasPendingChanges()) {
@@ -46,7 +102,34 @@ sap.ui.define(
         MessageToast.show(this._getText("refreshSuccessMessage"));
       },
 
-      onSearch: function () {
+      onSave() {
+        const fnSuccess = () => {
+          this._setBusy(false);
+          MessageToast.show(this._getText("changesSentMessage"));
+          this._setUIChanges(false);
+        };
+
+        const fnError = (oError) => {
+          this._setBusy(false);
+          this._setUIChanges(false);
+          MessageBox.error(oError.message);
+        };
+
+        this._setBusy(true); // Lock UI until submitBatch is resolved.
+        this.getView()
+          .getModel()
+          .submitBatch("peopleGroup")
+          .then(fnSuccess, fnError);
+        this._bTechnicalErrors = false; // If there were technical errors, a new save resets them.
+      },
+
+      onResetChanges() {
+        this.byId("peopleList").getBinding("items").resetChanges();
+        this._bTechnicalErrors = false;
+        this._setUIChanges();
+      },
+
+      onSearch() {
         const oView = this.getView(),
           sValue = oView.byId("searchField").getValue(),
           oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
@@ -57,7 +140,7 @@ sap.ui.define(
           .filter(oFilter, FilterType.Application);
       },
 
-      onSort: function () {
+      onSort() {
         const oView = this.getView(),
           aStates = [undefined, "asc", "desc"],
           aStateTextIds = ["sortNone", "sortAscending", "sortDescending"];
@@ -80,11 +163,55 @@ sap.ui.define(
         MessageToast.show(sMessage);
       },
 
-      _getText: function (sTextId, aArgs) {
+      onMessageBindingChange(oEvent) {
+        let aContexts = oEvent.getSource().getContexts(),
+          aMessages,
+          bMessageOpen = false;
+
+        if (bMessageOpen || !aContexts.length) {
+          return;
+        }
+
+        // Extract and remove the technical messages
+        aMessages = aContexts.map((oContext) => {
+          return oContext.getObject();
+        });
+        sap.ui.getCore().getMessageManager().removeMessages(aMessages);
+
+        this._setUIChanges(true);
+        this._bTechnicalErrors = true;
+
+        MessageBox.error(aMessages[0].message, {
+          id: "serviceErrorMessageBox",
+          onClose: () => {
+            bMessageOpen = false;
+          },
+        });
+
+        bMessageOpen = true;
+      },
+
+      _getText(sTextId, aArgs) {
         return this.getOwnerComponent()
           .getModel("i18n")
           .getResourceBundle()
           .getText(sTextId, aArgs);
+      },
+
+      _setUIChanges(bHasUIChanges) {
+        if (this._bTechnicalErrors) {
+          // If there is currently a technical error, then force 'true'.
+          bHasUIChanges = true;
+        } else if (bHasUIChanges === undefined) {
+          bHasUIChanges = this.getView().getModel().hasPendingChanges();
+        }
+        let oModel = this.getView().getModel("appView");
+        oModel.setProperty("/hasUIChanges", bHasUIChanges);
+      },
+
+      _setBusy: function (bIsBusy) {
+        var oModel = this.getView().getModel("appView");
+        oModel.setProperty("/busy", bIsBusy);
       },
     });
   }
